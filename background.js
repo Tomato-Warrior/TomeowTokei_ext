635591c483ab2ff2cb0e95aa9d2e89eea1bc67a0
Linda Wu <pinky510100@gmail.com>
2020-06-03 14:46:05 +0800

    feat/login, logout

--- background.js
+++ background.js
@@ -3,11 +3,16 @@ const ALARM_NAME = "tictac";
 class Tictac {
   constructor() {
     this.workTime = 1;
+    this.time = 1;
     this.restTime = 5;
     this.longRestTime = 15;
     this.timer = "work";
     this.isRun = false;
-
+    this.workNum = 0;
+    this.restNum = 0;
+    
+    // 預設icon背景顏色
+    chrome.browserAction.setBadgeBackgroundColor({ color: '#4285f4' })
     // 點擊icon時觸發事件
     //browser_action有default_popup時無法使用
     chrome.browserAction.onClicked.addListener(() => {this.handleIconClick()})
@@ -19,23 +24,21 @@ class Tictac {
 
   handleIconClick() {
     if (this.isRun) {
-      chrome.browserAction.setBadgeText({text: ""})
       this.stop()
     } else {
       this.start()
       this.workTime = 1
-      chrome.browserAction.setBadgeText({text: String(this.workTime)})
     }
     // 點擊icon後變更運轉狀態
     this.isRun = !this.isRun;
   }
 
   handleAlarm() {
-    if (this.workTime - 1 < 1) {
+    this.time--
+    if (this.time < 1) {
       this.stop()
-    }
-    if (this.timer === "work") {
-      this.workTime --
+      // chrome.runtime.openOptionsPage()
+    } else {
       chrome.browserAction.setBadgeText({text: String(this.workTime)})
     }
   }
@@ -45,25 +48,58 @@ class Tictac {
       delayInMinutes: 1,
       periodInMinutes: 1
     });
-    chrome.alarms.getAll((array) => {
-      console.log(array);
-    })
+    chrome.browserAction.setBadgeText({text: String(this.workTime)})
   }
   
   stop() {
+    this.isRun = false
     chrome.alarms.clear(ALARM_NAME);
     chrome.browserAction.setBadgeText({text: ""});
+  }
+
+  onMessage(request) {
+    if (request.func === 'getTimeInfo') {
+      return {
+        func: request.func,
+        timeType: this.timer,
+        restNum: this.restNum,
+        workNum: this.workNum
+      }
+    } else if (request.func === 'switchAlarm') {
+      this.switchAlarm()
+    }
+  }
+
+  switchAlarm() {
+    if (this.timer === 'work') {
+      chrome.browserAction.setBadgeBackgroundColor({ color: '#6e6e6e' })
+      this.workNum++
+      this.timer = 'rest'
+      if (this.workNum === 4) {
+        this.time = this.longRestTime
+      } else {
+        this.time = this.restTime
+      }
+      this.start()
+    } else {
+      chrome.browserAction.setBadgeBackgroundColor({ color: '#4285f4' })
+      this.restNum++
+      if (this.restNum === 4) {
+        this.workNum = 0
+        this.restNum = 0
+        // 不用this.timer = 'work'?
+        // 這兩行可以移除去嗎？
+        this.time = this.workTime
+      } else {
+        this.timer = 'work'
+        this.time = this.workTime
+      }
+      this.start()
+    }
   }
 }
 
 var tictac = new Tictac();
-
-chrome.runtime.openOptionsPage(callback)
-
-{
-  fun: 'getTimeInfo'
-  data: {}
-}
 
 chrome.runtime.onConnect.addListener(function(port) {
   if(port.name === "tictac") {
--- option.js
+++ option.js
@@ -1,8 +1,105 @@
+var btns = document.getElementsByClassName('btn')
+
+Array.prototype.map.call(btns, node => {
+  node.addEventListener('click', switchAlarm)
+})
+
 var port = chrome.runtime.connect({name:'tictac'})
 
 port.onMessage.addListener(function(response) {
-  if(response.fun === 'getTimeInfo') {
-    document.getElementById(response.timeType).style.display = "block"
-    document.getElementById(response.timeType === "work" ? 'rest' : 'work').style.display = 'none'
+  console.log(response.timeType)
+  if(response.func === 'getTimeInfo') {
+    if (response.timeType === 'rest' && response.restNum === 3) {
+      document.getElementsById('finish').style.display = 'block'
+      document.getElementsById('work').style.display = 'none'
+      document.getElementsById('rest').style.display = 'none'
+    } else {
+    document.getElementById(response.timeType === 'work' ? 'work' : 'rest').style.display = 'block'
+    document.getElementById(response.timeType === 'work' ? 'rest' : 'work').style.display = 'none'
+    }
   }
 })
+
+port.postMessage({func: 'getTimeInfo'})
+
+function switchAlarm() {
+  port.postMessage({func: 'switchAlarm'})
+  window.close()
+}
+
+
+
+// handle login
+
+function getLoginValue() {
+  const email = document.querySelector('#email');
+  const password = document.querySelector('#password');
+  return { email: email.value, password: password.value };
+}
+
+function postData(url, data) {
+  return fetch(url, {
+    body: JSON.stringify(data),
+    cache: "no-cache",
+    method: "POST",
+    redirect: "follow",
+    referrer: "no-referrer",
+    mode: "cors",
+    headers: {
+      "user-agent": "Mozilla/4.0 MDN Example",
+      "content-type": "application/json",
+    },
+  }).then(res => res.json());
+}
+
+const form = document.querySelector('form');
+ 
+form.addEventListener('submit', function loginJson(e) {
+  e.preventDefault();
+  postData("http://127.0.0.1:3000/api/v1/login", getLoginValue())
+    .then((data) => {
+      localStorage.setItem("auth_token", data["auth_token"]);
+      if (data["message"] === "ok") {
+        loginPage(true);
+      }
+      // notice login success 
+    })
+    .catch((error) => {
+      // notice no account record
+    });
+});
+
+function loginPage(isLogin) {
+  let logincontainer = document.querySelector('.logincontainer');
+  console.log(logincontainer)
+  let tomato = document.querySelector('.tomato');
+  if (isLogin) {
+    logincontainer.classList.add("d-none");
+    tomato.classList.remove("d-none");
+  } else {
+    localStorage.clear();
+    logincontainer.classList.remove("d-none");
+    tomato.classList.add("d-none");
+  }
+}
+
+// handle logout
+
+// function getLogoutValue() {
+//   let logout = localStorage.getItem("auth_token");
+//   return { auth_token: logout };
+// }
+
+let logout = document.querySelector('.logoutcontainer');
+logout.addEventListener('click', function logoutjson(e) {
+  e.preventDefault();
+  let logoutvalue = localStorage.getItem("auth_token");
+  postData("http://127.0.0.1:3000/api/v1/logout", { auth_token: logoutvalue })
+    .then((data) => {
+      console.log(data)
+      loginPage(false);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+});
